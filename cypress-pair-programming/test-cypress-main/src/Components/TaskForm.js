1c49e634496c33243bcf737e2d7410f18b8dbe52
pankaj-404 <pankajbhayre@gmail.com>
2020-07-04 16:24:57 +0530

    Test-Cypress-Delete

---
+++ cypress-pair-programming/test-cypress-main/src/Components/TaskForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TaskForm = props =>
+  <form onSubmit={props.onSubmit}>
+    <input
+      type='text'
+      autoFocus
+      value={props.value}
+      onChange={props.onChange}
+      className="p-2 rounded border task-input"
+      placeholder="Add something?"/>
+  </form>
+
+  export default TaskForm
